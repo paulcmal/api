c92c8b776e9c9bd70978cb0ac0806145ebedfe0c
cmal <paul@cmal.info>
2018-03-07 14:05:37 +0100

    Optional trailing slash + don't crash when subroute is empty

--- src/Api.php
+++ src/Api.php
@@ -8,14 +8,14 @@ class Api {
     public function __construct($backends) {
         
         $this->backends = $backends;
-
         $routes = [];
         foreach (['GET'] as $method) {
             $routes[$method] = [
 				// A backend should always have some params passed to
-            	'/{backend}/{args:.+}' => [$this, 'toBackend']
+            	'/{backend}[/[{args:.+}]]' => [$this, 'toBackend']
 			];
         }
+        
         new Router ($routes);
     }
 
@@ -24,7 +24,9 @@ class Api {
 		
 		// We stripped the leading '/' to match the route in __construct
 		// So now we need to put it back in place
-		$params['args'] = '/' . $params['args'];
+		$params['args'] = '/' . (empty($params['args']) ? '' : $params['args']);
+		
+		//var_dump($params['args']);
 
         if (array_key_exists($backend, $this->backends)) {
             call_user_func([$this->backends[$backend], 'dispatch'], $params['args']);
